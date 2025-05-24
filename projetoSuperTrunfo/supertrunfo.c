7853f6240082a4a1edafdcdd7cb2aaf75ad6f0f3
ViniMargo <vinicius.contact2@gmail.com>
2025-05-24 15:16:51 -0300

    wip - progresso automático

--- projetoSuperTrunfo/supertrunfo.c
+++ projetoSuperTrunfo/supertrunfo.c
@@ -4,29 +4,33 @@
 int main(){
 
     // Declaração das variaveis
-    char estado;
-    char codigoDaCarta[4];
-    char nomeDaCidade[24];
+    char estado, estado2;
+    char codigoDaCarta[4], codigoDaCarta2[4]; 
+    char nomeDaCidade[24], nomeDaCidade2[24]; 
 
-    int populacao;
-    int NumeroDePontosTuristicos;
+    int populacao, populacao2;
+    int NumeroDePontosTuristicos, NumeroDePontosTuristicos2;
 
-    float AreaPorKm2;
-    float PIB;
+    float AreaPorKm2, AreaPorKm22;
+    float PIB, PIB2;
 
-    float densidadeDemografica;
-    float pibPerCapita;
+    float densidadeDemografica, densidadeDemografica2;
+    float pibPerCapita, pibPerCapita2;
 
     // Inicializando variaveis (Redundancia, mas para fins de exemplo)
     estado = 'A';
     codigoDaCarta[1] = '0'; // Inicializa o segundo caractere do código da carta como 0 (Ex: A01, A02, A03, A04)
+    codigoDaCarta2[1] = '0';
     strcpy(nomeDaCidade, "São Paulo"); // Inicializa um Array com uma string
     populacao = 12325000;
     NumeroDePontosTuristicos = 50;
     AreaPorKm2 = 1521.11;
     PIB = 699.28;
 
-    // Entrada de dados
+
+    // Entrada de dados Carta 1
+    printf("\n\nDigite os dados da segunda carta:\n\n");
+
     printf("Digite o estado: ");
     scanf(" %c", &estado);
 
@@ -38,7 +42,7 @@ int main(){
     printf("Digite o nome da cidade: ");
     fgets(nomeDaCidade, sizeof(nomeDaCidade), stdin); // Lê o nome das cidades com espaço (Ex: São Paulo)
                                                       // O fgets lê até o \n, então não precisa de \n no final
-
+                                                      
     printf("Digite a população: ");
     scanf("%d", &populacao);
 
@@ -57,10 +61,37 @@ int main(){
                                                    // Por exemplo, se a população for 10 e a área for 2, 
                                                   // a densidade demográfica é 5 habitantes/km² 10p/2km²
 
-    // Exibição dos dados
-    printf("\n");
-    printf("EXIBIÇÃO DOS DADOS:\n");
+    // Entrada de dados Carta 2
+    printf("\n\nDigite os dados da segunda carta:\n\n");
 
+    printf("Digite o estado: ");
+    scanf(" %c", &estado2);
+
+    codigoDaCarta2[0] = estado2;
+    printf("Digite o código da carta (1 a 4): ");
+    scanf(" %c", &codigoDaCarta2[2]); 
+    getchar();
+
+    printf("Digite o nome da cidade: ");
+    fgets(nomeDaCidade2, sizeof(nomeDaCidade2), stdin);
+
+    printf("Digite a população: ");
+    scanf("%d", &populacao2);
+
+    printf("Digite o número de pontos turísticos: ");
+    scanf("%d", &NumeroDePontosTuristicos2);
+
+    printf("Digite a área em km²: ");
+    scanf("%f", &AreaPorKm22);
+
+    printf("Digite o PIB: ");
+    scanf("%f", &PIB2);
+
+    //Processamento dos dados carta 2
+    pibPerCapita2 = PIB2 / populacao2; 
+    densidadeDemografica2 = populacao2 / AreaPorKm22; 
+
+    // Exibição dos dados carta 1
     printf("\nEstado: %c\n", estado);
     printf("Código da carta: %s\n", codigoDaCarta);
     printf("Nome da cidade: %s", nomeDaCidade); // Não precisa de \n, pois já tem no fgets
