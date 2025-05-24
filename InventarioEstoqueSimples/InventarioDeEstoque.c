160450c6d6977d4a7871aa1ad12624880a1c1135
ViniMargo <vinicius.contact2@gmail.com>
2025-05-24 03:01:28 -0300

    InventarioEstoqueSimples e SuperTrunfo

---
+++ InventarioEstoqueSimples/InventarioDeEstoque.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    // Declarar as variaveis
+    char produtoa[30] = "Produto A";
+    char produtob[30] = "Produto B";
+
+        // o unsigned int é um inteiro sem sinal, ou seja, não pode ser negativo
+    unsigned int estoqueb = 2000;
+    unsigned int estoquea = 1000; 
+
+        // o estoque mínimo é o mínimo de produtos que devem estar em estoque
+    unsigned int estoqueMinimoa = 500; 
+    unsigned int estoqueMinimob = 2500;
+
+        // o estoque máximo é o máximo de produtos que podem estar em estoque
+    float precoprodutoa = 10.50;
+    float precoprodutob = 20.50;
+    
+        // o valor do estoque é o valor total do estoque, ou seja, o preço unitário multiplicado pela quantidade em estoque
+    double valorEstoqueA;
+    double valorEstoqueB;
+
+        // Variaveis para armazenar o resultado da comparação
+    int resultadoA, resultadoB;
+    int comparacaoValorEstoqueA, comparacaoValorEstoqueB;;
+
+    // Exibir os dados do produto A e B
+    printf("Produto: %s tem %u e o valor unitario é R$%.2f\n", produtoa, estoquea, precoprodutoa);
+    printf("Produto: %s tem %u e o valor unitario é R$%.2f\n", produtob, estoqueb, precoprodutob);
+    printf("\n");
+
+    // Comparar com o estoque mínimo
+    resultadoA = estoquea < estoqueMinimoa;
+    resultadoB = estoqueb < estoqueMinimob;
+
+    // Exibir o resultado da comparação
+        /* EXPLICAÇÃO DO OPERADOR TENÁRIO: "?":
+            Estrutura: condição ? se sim : se não (0 : 1)
+            Se resultadoB for 1 imprime 1 (Esquerdo), se for 0 imprime "Não"(Direito)
+            O operador ternário é uma forma de simplificar o if-else, onde se a condição for verdadeira, imprime "Sim" (if), caso contrário imprime "Não" (else)
+        */
+    printf("Produto: %s tem estoque abaixo do mínimo? %s\n", produtoa, resultadoA ? "Sim" : "Não");
+    printf("Produto: %s tem estoque abaixo do mínimo? %s\n", produtob, resultadoB ? "Sim" : "Não"); 
+    printf("\n");
+    // Calcular o valor total do estoque
+    valorEstoqueA = estoquea * precoprodutoa; // valor do estoque A é multiplicado pelo preço unitário do produto A (Ex: produtoa = 1000 * valorDoProdutoA = 10.50 = 10500)
+    valorEstoqueB = estoqueb * precoprodutob; // (Ex: produtob = 2000 * valorDoProdutoB = 20.50 = 41000)
+    
+    // comparar o valor do estoque
+    comparacaoValorEstoqueA = valorEstoqueA < valorEstoqueB; // 41000 < 10500 = 0 (Falso)
+    comparacaoValorEstoqueB = valorEstoqueB < valorEstoqueA; // 10500 < 41000 = 1 (Verdadeiro)
+
+    printf("O valor total do %s (R$%.2f) é menor que o produto B? %s\n", produtoa, valorEstoqueA, comparacaoValorEstoqueA ? "Sim" : "Não"); 
+    printf("O valor total do %s (R$%.2f) é menor que o produto A? %s\n", produtob, valorEstoqueB, comparacaoValorEstoqueB ? "Sim" : "Não");
+    printf("\n");
+
+    return 0;
+}
---
+++ projetoSuperTrunfo/supertrunfo.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+
+    // Declaração das variaveis
+    char estado;
+    char codigoDaCarta[4];
+    char nomeDaCidade[24];
+
+    int populacao;
+    int NumeroDePontosTuristicos;
+
+    float AreaPorKm2;
+    float PIB;
+
+    float densidadeDemografica;
+    float pibPerCapita;
+
+    // Inicializando variaveis (Redundancia, mas para fins de exemplo)
+    estado = 'A';
+    codigoDaCarta[1] = '0'; // Inicializa o segundo caractere do código da carta como 0 (Ex: A01, A02, A03, A04)
+    strcpy(nomeDaCidade, "São Paulo"); // Inicializa um Array com uma string
+    populacao = 12325000;
+    NumeroDePontosTuristicos = 50;
+    AreaPorKm2 = 1521.11;
+    PIB = 699.28;
+
+    // Entrada de dados
+    printf("Digite o estado: ");
+    scanf(" %c", &estado);
+
+    codigoDaCarta[0] = estado; // O primeiro caractere do código da carta é o estado
+    printf("Digite o código da carta (1 a 4): ");
+    scanf(" %c", &codigoDaCarta[2]); // O terceiro caractere do código da carta é o número da carta (1 a 4)
+    getchar(); // Limpa o buffer do teclado
+
+    printf("Digite o nome da cidade: ");
+    fgets(nomeDaCidade, sizeof(nomeDaCidade), stdin); // Lê o nome das cidades com espaço (Ex: São Paulo)
+                                                      // O fgets lê até o \n, então não precisa de \n no final
+
+    printf("Digite a população: ");
+    scanf("%d", &populacao);
+
+    printf("Digite o número de pontos turísticos: ");
+    scanf("%d", &NumeroDePontosTuristicos);
+
+    printf("Digite a área em km²: ");
+    scanf("%f", &AreaPorKm2);
+
+    printf("Digite o PIB: ");
+    scanf("%f", &PIB);
+
+    //Processamento dos dados
+    pibPerCapita = PIB / populacao; // pib dividido pela população
+    densidadeDemografica = populacao / AreaPorKm2;  // Quantidade de pessoas por km² em uma ArePorkm2
+                                                   // Por exemplo, se a população for 10 e a área for 2, 
+                                                  // a densidade demográfica é 5 habitantes/km² 10p/2km²
+
+    // Exibição dos dados
+    printf("\n");
+    printf("EXIBIÇÃO DOS DADOS:\n");
+
+    printf("\nEstado: %c\n", estado);
+    printf("Código da carta: %s\n", codigoDaCarta);
+    printf("Nome da cidade: %s", nomeDaCidade); // Não precisa de \n, pois já tem no fgets
+    printf("População: %d\n", populacao);
+    printf("Número de pontos turísticos: %d\n", NumeroDePontosTuristicos);
+    printf("Área em km²: %.2f\n", AreaPorKm2);
+    printf("PIB: %.2f\n", PIB);
+    printf("Densidade demográfica: %.2f habitantes/km²\n", densidadeDemografica);
+    printf("PIB per capita: %.2f\n", pibPerCapita);
+
+    return 0;
+}
