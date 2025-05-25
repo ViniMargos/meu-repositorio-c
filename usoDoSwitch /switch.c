bf40c6d9375518d4513e54c0a2c8cd9fe77fa050
ViniMargo <vinicius.contact2@gmail.com>
2025-05-24 23:43:53 -0300

    wip - usoDoSwitch

--- usoDoSwitch /switch.c
+++ usoDoSwitch /switch.c
@@ -11,7 +11,7 @@ int main() {
     {
         case 1: // Se a variável for igual a 1
             printf("Você escolheu a opção 1\n");
-            break; // O break é usado para sair do switch, se não usar o break, ele continua executando os próximos casos
+            break;
         case 2: // Se a variável for igual a 2
             printf("Você escolheu a opção 2\n");
             break;
@@ -22,4 +22,15 @@ int main() {
             printf("Opção inválida\n");
     }
 
+    // Estrutura do switch:
+
+    switch (expression) // A expressão pode ser um valor inteiro, caractere ou uma expressão que resulte em um desses tipos
+    {
+    case constant expression: // Se a expressão for igual a constante, executa o bloco de código correspondente
+        /* code */
+        break; // O break é usado para sair do switch, se não usar o break, ele continua executando os próximos casos
+    
+    default: // O default é opcional e é executado se nenhum dos casos acima for verdadeiro
+        break;
+    }
 }
